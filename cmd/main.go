ae53078a70977a06f62d1674a3d3362445b659aa
Simon Sandvik Lee <simonsandviklee@gmail.com>
2025-04-01 00:54:01 +0200

    feat: Add main

---
+++ cmd/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+
+	"github.com/CogitoNTNU/cogi-go/internal/config"
+	"github.com/CogitoNTNU/cogi-go/internal/db"
+	"github.com/CogitoNTNU/cogi-go/internal/util/env"
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+  r := gin.Default()
+
+  ctx := context.Background()
+
+  cfg := config.LoadApiConfig()
+
+  logger := logrus.New().WithField("app", cfg.AppName).WithContext(ctx).WithField("router", r)
+
+  logger.Info("Starting server")
+
+  env := env.Configure(".env")
+
+  db, err := db.InitDb(env, logger, ctx)
+  if err != nil {
+    logger.WithError(err).Fatal("Failed to connect to db")
+  }
+
+
+
+
+  r.GET("/ping", func(c *gin.Context) {
+    c.JSON(200, gin.H{
+      "message": "pong",
+    })
+  })
+  r.Run()
+}
