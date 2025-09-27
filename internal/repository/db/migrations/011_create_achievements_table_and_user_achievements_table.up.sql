129e093002115596ef7b92980b509184af8ceb6b
A1ice-Z <alicezheng888@gmail.com>
2025-09-27 20:06:31 +0200

    feat: :sparkles: create migration for achievements table

---
+++ internal/repository/db/migrations/011_create_achievements_table_and_user_achievements_table.up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS achievements(
+    achievement_id serial PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    icon_url VARCHAR(300) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS user_achievements(
+    user_achievement_id serial PRIMARY KEY,
+    user_id INT REFERENCES users(user_id) ON DELETE CASCADE,
+    achievement_id INT REFERENCES achievements(achievement_id) ON DELETE CASCADE,
+    date_earned TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
