589d7269e8007789d63ca66e5b52798fc6140d03
A1ice-Z <alicezheng888@gmail.com>
2025-09-27 20:03:09 +0200

    feat: :sparkles: create migration for projects table

---
+++ internal/repository/db/migrations/002_create_projects_table.up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS projects(
+   project_id serial PRIMARY KEY,
+   title VARCHAR (300) NOT NULL,
+   github_url VARCHAR (300) NOT NULL,
+   logo VARCHAR (300) NOT NULL,
+   playable BOOLEAN NOT NULL,
+   released BOOLEAN NOT NULL,
+   active_project BOOLEAN NOT NULL,
+   project_url VARCHAR (300)
+);
