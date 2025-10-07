5553fa55bf36adb2a78697d0a4deefdc23e40516
A1ice-Z <alicezheng888@gmail.com>
2025-10-07 18:27:01 +0200

    feat: :sparkles: create article model

---
+++ internal/model/article.go
@@ -0,0 +1,16 @@
+package model
+
+import (
+	"time"
+	"github.com/google/uuid"
+)
+
+type Article struct {
+	Id        uuid.UUID
+	Title     string
+	Content   []byte
+	AuthorId  uuid.UUID
+	ProjectId uuid.UUID
+	CreatedAt *time.Time
+	UpdatedAt *time.Time
+}
