98d134a997a59006429755c6de6ebcb2b28edfa9
A1ice-Z <alicezheng888@gmail.com>
2025-10-07 18:27:17 +0200

    feat: :sparkles: create application model

---
+++ internal/model/application.go
@@ -0,0 +1,18 @@
+package model
+
+import (
+	"time"
+	"github.com/google/uuid"
+)
+
+type Application struct {
+	Id              uuid.UUID
+	UserId          uuid.UUID
+	ProjectId1     uuid.UUID
+	ProjectId2     uuid.UUID
+	ProjectId3     uuid.UUID
+	ApplicationText string
+	AppliedAt      *time.Time
+	ModifiedAt     *time.Time
+	IsMember       bool
+}
