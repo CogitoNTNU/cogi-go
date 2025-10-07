6c6ddb32cedfa438f6b4d5be0cccef22460671fb
A1ice-Z <alicezheng888@gmail.com>
2025-10-07 18:29:13 +0200

    feat: :sparkles: create achivement model

---
+++ internal/model/achivement.go
@@ -0,0 +1,20 @@
+package model
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Achivement struct {
+	Id          uuid.UUID
+	Title      string
+	IconUrl     *string
+}
+
+type UserAchivement struct {
+	Id          uuid.UUID
+	UserId      uuid.UUID
+	AchivementId uuid.UUID
+	DateEarned  *time.Time
+}
