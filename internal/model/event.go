d6a2ed55af03e5aca9eec96e826b30c320241e9c
A1ice-Z <alicezheng888@gmail.com>
2025-11-12 10:31:22 +0100

    feat: :sparkles: create event and event registration models Co-authored-by: Christopher Høe <Christio02@users.noreply.github.com>

--- internal/model/event.go
+++ internal/model/event.go
@@ -9,26 +9,23 @@ import (
 type EventType string
 
 const (
-	WorkshopEventType EventType = "workshop"
-	NewsEventType EventType = "news"
+	WorkshopEventType  EventType = "workshop"
+	NewsEventType      EventType = "news"
 	HackathonEventType EventType = "hackathon"
-	MeetingEventType EventType = "meeting"
-	OtherEventType EventType = "other"
-
-
+	MeetingEventType   EventType = "meeting"
+	OtherEventType     EventType = "other"
 )
 
 type Event struct {
-	Id uuid.UUID
-	Name string
-	StartAt time.Time
-	EndAt time.Time
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	Type EventType
-	Location string
-	Description string
-	Content []byte
-	MaxAttendees int64
-	Attendees []User
+	Id           uuid.UUID
+	Name         string
+	StartAt      time.Time
+	EndAt        time.Time
+	CreatedAt    time.Time
+	UpdatedAt    time.Time
+	Type         EventType
+	Location     string
+	Description  string
+	Content      []byte
+	MaxAttendees int
 }
--- internal/model/eventRegistration.go
+++ internal/model/eventRegistration.go
@@ -6,26 +6,10 @@ import (
 	"github.com/google/uuid"
 )
 
-type RegistrationStatus string
-
-const (
-    RegistrationRequested RegistrationStatus = "requested"
-    RegistrationConfirmed RegistrationStatus = "confirmed"
-    RegistrationWaitlisted RegistrationStatus = "waitlisted"
-    RegistrationCancelled RegistrationStatus = "cancelled"
-)
-
 type EventRegistration struct {
 	ID uuid.UUID
 	EventId uuid.UUID
 	UserId uuid.UUID
 
-	Status RegistrationStatus
-
  	CreatedAt time.Time
-    UpdatedAt time.Time
-
-    Event *Event
-    User *User
-
 }
