a6d3f787efa23462d4ec43ef6e87132a8e2b046f
A1ice-Z <alicezheng888@gmail.com>
2025-11-13 19:06:05 +0100

    feat: :sparkles: create handler layer for event Co-authored-by: Christopher Høe <Christio02@users.noreply.github.com>

---
+++ internal/handler/eventHandler.go
@@ -0,0 +1,28 @@
+package handler
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/CogitoNTNU/cogi-go/internal/service"
+	"github.com/gin-gonic/gin"
+)
+
+type Event struct {
+    service *service.Event
+    ctx *context.Context
+}
+
+func NewEventHandler(eventService *service.Event, ctx *context.Context) *Event {
+    return &Event{service: eventService, ctx: ctx}
+}
+
+func (e *Event) GetAllEvents(gCtx *gin.Context) {
+    allEvents, err := e.service.GetAllEvents(e.ctx)
+    if err != nil {
+        gCtx.AbortWithStatusJSON(err.Code, err.Message)
+        return
+    }
+
+    gCtx.JSON(http.StatusOK, allEvents)
+}
