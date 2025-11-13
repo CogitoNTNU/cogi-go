294662945c2271290988245780b5c194939dce98
A1ice-Z <alicezheng888@gmail.com>
2025-11-13 19:05:18 +0100

    feat: :sparkles: create repository for event Co-authored-by: Christopher Høe <Christio02@users.noreply.github.com>

---
+++ internal/repository/event/queries.go
@@ -0,0 +1,25 @@
+package eventRepository
+
+const (
+	qGetEvents = `SELECT event_id, name, start_at, end_at, type, location, description, max_atendees
+        FROM EVENT;
+    `
+	qGetEvent = `SELECT * FROM EVENT WHERE event_id = :eventId`
+
+	qCreateEvent = `INSERT INTO EVENT (event_id, name, start_at, created_at, updated_at, type, location, description, content, max_atendees) VALUES
+        (
+            :eventId,
+            :name,
+            :startAt,
+            :createdAt,
+            :updatedAt,
+            :type,
+            :location,
+            :description,
+            :content,
+            :maxAtendees
+        )
+    
+     `
+	qDeleteEvent = `DELETE FROM EVENT WHERE event_id = :eventId`
+)
---
+++ internal/repository/event/repo.go
@@ -0,0 +1,68 @@
+package eventRepository
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/CogitoNTNU/cogi-go/internal/model"
+	"github.com/CogitoNTNU/cogi-go/internal/repository/db"
+	"github.com/jmoiron/sqlx"
+	"github.com/sirupsen/logrus"
+)
+
+type Repo struct {
+	db                *sqlx.DB
+	queries           queries
+	queryTimeOutLimit time.Duration
+	log               logrus.FieldLogger
+}
+
+func NewRepo(db *sqlx.DB, queryTimeOutLimit time.Duration, logger *logrus.Entry) *Repo {
+	queries, err := PreparedQueries(db)
+
+	if err != nil {
+		logger.Fatalf("failed to prepare queries: %s", err)
+	}
+
+	return &Repo{
+		db:                db,
+		queries:           queries,
+		queryTimeOutLimit: queryTimeOutLimit,
+		log:               logger,
+	}
+}
+
+func (r *Repo) GetAllEvents(ctx *context.Context) ([]model.Event, *model.ErrorResponse) {
+	cCtx, cancel := context.WithTimeout(*ctx, r.queryTimeOutLimit)
+	defer cancel()
+
+	var eventResults []db.Event
+
+	args := map[string]any{}
+	err := r.queries.Read.getEvents.SelectContext(cCtx, &eventResults, args)
+
+	if err != nil {
+		return nil, &model.ErrorResponse{
+			Code:    http.StatusInternalServerError,
+			Message: err.Error(),
+		}
+	}
+
+	events := make([]model.Event, 0, len(eventResults))
+	for _, eventResult := range eventResults {
+		event := eventResult.ToModel()
+		events = append(events, *event)
+	}
+
+	return events, nil
+}
+
+func (r *Repo) Close() (err error) {
+	err = r.queries.Close()
+	if err != nil {
+		return fmt.Errorf("error closing queries: %w", err)
+	}
+	return
+}
