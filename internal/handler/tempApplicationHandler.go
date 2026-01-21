a80314cc6d0f90809c6dcff13424de5423140e54
Simon Sandvik Lee <simonsandviklee@gmail.com>
2026-01-21 14:55:42 +0100

    feat: Update temp application handler with email support

--- internal/handler/tempApplicationHandler.go
+++ internal/handler/tempApplicationHandler.go
@@ -2,12 +2,16 @@ package handler
 
 import (
 	"context"
+	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/CogitoNTNU/cogi-go/internal/api/dto"
 	"github.com/CogitoNTNU/cogi-go/internal/service"
 	"github.com/CogitoNTNU/cogi-go/internal/util/env"
 	"github.com/gin-gonic/gin"
+
+	mail "github.com/xhit/go-simple-mail/v2"
 )
 
 type TempApplication struct {
@@ -29,6 +33,23 @@ func (t *TempApplication) CreateTempApplication(gCtx *gin.Context) {
 	err := t.service.CreateTempApplication(t.ctx, &req)
 	if err != nil {
 		gCtx.AbortWithStatusJSON(err.Code, err.Message)
+		return
+	}
+
+	smtpClient := gCtx.MustGet("smtp_client").(*mail.SMTPClient)
+	email := mail.NewMSG()
+	email.SetFrom("From noreply@cogito-ntnu.no <noreply@cogito-ntnu.no>").AddTo(req.Email).SetSubject("Thank you for your project application to Cogito NTNU!")
+
+	body := fmt.Sprintf("Dear %s,\n\nThank you for your application to join Cogito NTNU!\n We have registered your wish to join the projects: %s. Please send us a mail at styret@cogito-ntnu.no if something is wrong.\n\nBest Regards,\nCogito NTNU Board", req.FirstName, strings.Join(req.Projects, ", "))
+	email.SetBody(mail.TextHTML, body)
+
+	if email.Error != nil {
+		gCtx.JSON(http.StatusInternalServerError, "Something went wrong when creating the reply email.")
+		return
+	}
+
+	if email.Send(smtpClient) != nil {
+		gCtx.JSON(http.StatusInternalServerError, "Something went wrong when sending the reply email.")
 		return
 	}
 
