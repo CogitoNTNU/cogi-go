9cc4a793e3e65ec7dd4cb8c50e5ef13455e6bda9
Edvard Klavenes <edvardklavenes@gmail.com>
2026-04-14 19:59:59 +0200

    feat: adds models for event and reg

---
+++ internal/model/eventRegistration.go
@@ -0,0 +1,15 @@
+package model
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type EventRegistration struct {
+	ID      uuid.UUID
+	EventId uuid.UUID
+	UserId  uuid.UUID
+
+	CreatedAt time.Time
+}
