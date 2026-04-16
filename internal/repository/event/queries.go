ba4d961a74b49ffb5fa77cb4c2aaa66fcb012701
Edvard Klavenes <edvardklavenes@gmail.com>
2026-04-16 19:01:28 +0200

    bug: adds end_at field

--- internal/repository/event/queries.go
+++ internal/repository/event/queries.go
@@ -6,11 +6,12 @@ const (
     `
 	qGetEvent = `SELECT * FROM EVENT WHERE event_id = :eventId`
 
-	qCreateEvent = `INSERT INTO EVENT (event_id, name, start_at, created_at, updated_at, type, location, description, content, max_atendees) VALUES
+	qCreateEvent = `INSERT INTO EVENT (event_id, name, start_at, end_at, created_at, updated_at, type, location, description, content, max_atendees) VALUES
         (
             :eventId,
             :name,
             :startAt,
+						:endAt,
             :createdAt,
             :updatedAt,
             :type,
